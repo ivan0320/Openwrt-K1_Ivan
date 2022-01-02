f01d24d0143465755acb744c8e8ba2914f778a0c
ivan0320 <80611691+ivan0320@users.noreply.github.com>
2022-01-02 23:01:42 +0800

    psg1208 修改为16M内存

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,9 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
+# 修改mt7620 固件配置文件
+# psg1208 修改为16M内存
+ mv images/mt7620.mk openwrt/target/linux/ramips/image/mt7620.mk
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
