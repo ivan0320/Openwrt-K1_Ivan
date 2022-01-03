a198af0c309b88975b1f352868a966ffeea00031
ivan0320 <80611691+ivan0320@users.noreply.github.com>
2022-01-03 11:34:27 +0800

    调整主题，和psg1208配置文件

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,16 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+# 拉取 argon 原作者的源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+# 修改mt7620 固件配置文件
+# psg1208 修改为16M内存
+ mv $GITHUB_WORKSPACE/images/mt7620.mk target/linux/ramips/image/mt7620.mk
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
